a10b773dd5e03eb013f91ea95e6b3f192bba9815
Keremcan Seker <kerem.4555@gmail.com>
2022-04-01 17:04:57 +0300

    hw1 q1 debug

--- PROGRAMMING/Hw/week1_hw-Lintorn-main/152120201074_HW1_q1.cpp
+++ PROGRAMMING/Hw/week1_hw-Lintorn-main/152120201074_HW1_q1.cpp
@@ -20,16 +20,32 @@ int main() {
 
 	}
 	// Check if there are any same value in arrB than change it to -1
-	for (int i = 0;i < 5;i++) {
-		int value;
-		value = B[i];
-		if (value == B[i + 1]) {
+	/* for (int i = 0;i < 5;i++) {
+		// int value;
+		// value = B[i];
+		if (B[i] == B[i + 1]) {
 			B[i] = -1;
 		}
 
-	}
+	} this loop only finds the same value if it they are next to each other */
 
-	//
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i+1; j < 5; j++)
+		{
+			if(B[i]==B[j]){
+				B[j] = -1;
+			}
+		}
+		
+	} /* this loops works like this  b0 b1,, b0 b2,, b0 b3,, b0 b4/
+
+	
+
+
+
+
+
 	for (size_t i = 0; i < 5; i++)
 	{
 		cout << B[i] << ' ';
